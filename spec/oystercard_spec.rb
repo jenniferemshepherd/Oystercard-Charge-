082e4b06c9acd93551a296e8989152634b18591f
alextwilson <github@alextwilson.co.uk>
2018-01-08 15:47:25 +0000

    Card has a balance that starts at 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  subject(:card) { Oystercard.new }
+
+  it 'has an initial balance of zero' do
+    expect(card.balance).to eq 0
+  end
 end
