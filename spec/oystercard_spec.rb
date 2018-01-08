b4a30fadcbfb9ed33659d868446853ceb7c55a4d
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-08 16:08:25 +0000

    Topup method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,13 @@ describe Oystercard do
   it 'has an initial balance of zero' do
     expect(card.balance).to eq 0
   end
+
+  describe '#topup' do
+
+    it 'increments balance by amount' do
+      expect{ card.topup(10) }.to change{ card.balance }.by 10
+    end
+
+  end
+
 end
